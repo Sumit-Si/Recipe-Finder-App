203b13e3891e7c40c7126f7c5843aeff1cfa3d3a
Sumit <pankajsinghtomar987@gmail.com>
2025-08-04 19:47:12 +0530

    complete recipe-finder v1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const searchForm = document.querySelector("#searchForm");
 const mealsContainer = document.querySelector("#meals-container");
 const errorMsg = document.querySelector("#errMsg");
 const loader = document.querySelector(".loader");
+const recipeModalContainer = document.querySelector("#recipe-modal");
 let renderData = "";
 let renderModalData = "";
 
@@ -19,7 +20,6 @@ async function handleSearch(e) {
     `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`
   );
   const { meals } = await res.json();
-  console.log(meals, "data");
 
   if (meals?.length > 0) {
     meals.map((meal) => {
@@ -27,6 +27,17 @@ async function handleSearch(e) {
     });
 
     mealsContainer.innerHTML = renderData;
+
+    document.querySelectorAll(".modal-btn").forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const mealId = btn.getAttribute("data-id");
+        const mealData = meals.find((meal) => meal.idMeal === mealId);
+        console.log(mealData, "mealDAta");
+
+        if (mealData) recipeModal(mealData);
+      });
+    });
+
     errorMsg.classList.add("hidden");
   } else {
     mealsContainer.innerHTML = "";
@@ -42,21 +53,21 @@ function renderMealsData(meal) {
     <div
             class="card border h-64 rounded-lg shadow-lg shadow-gray-800 bg-gray-800 p-2 relative flex items-center justify-end flex-col"
             style="
-              background: url('${meal.strMealThumb}');
+              background: url('${meal?.strMealThumb}');
               background-position: center center;
               background-size: cover;
               opacity: 0.85;
             "
           >
             <div class="bg-gray-900/80 w-full rounded-lg px-3 py-2">
-              <h3 class="font-bold mb-1.5">${meal.strMeal}</h3>
+              <h3 class="font-bold mb-1.5">${meal?.strMeal} &CenterDot; ${meal?.strArea}</h3>
               <div class="flex gap-3 items-center mb-4">
                 <div class="text-sm ${
-                  meal.strCategory === "Vegetarian"
+                  meal?.strCategory === "Vegetarian"
                     ? "text-green-500"
-                    : "text-red-500"
-                }">${meal.strCategory}</div>
-                <div class="flex gap-2">${
+                    : meal?.strCategory === "Dessert" ? "text-green-500" :  "text-red-500"
+                }">${meal?.strCategory}</div>
+                <div class="flex gap-2 overflow-x-scroll py-1 px-2 transition hide-scrollbar">${
                   meal?.strTags
                     ? meal.strTags
                         ?.toString()
@@ -64,14 +75,16 @@ function renderMealsData(meal) {
                         .map(
                           (tag) =>
                             `<span class="text-xs ring-2 ring-rose-600 py-0.5 px-1 rounded-full">${tag}</span>`
-                        )
+                        ).slice(0,3)
                         .join(" ")
                     : ""
                 }</div>
               </div>
               <div class="text-center">
 
-                  <button class="text-xs font-semibold border py-1 px-3 hover:bg-gray-100 hover:text-black rounded-lg cursor-pointer" id="view" onclick="recipeModal()">
+                  <button class="text-xs modal-btn font-semibold border py-1 px-3 hover:bg-gray-100 hover:text-black rounded-lg cursor-pointer" data-id="${
+                    meal?.idMeal
+                  }" onclick="recipeModal()">
                     View
                   </button>
               </div>
@@ -79,14 +92,28 @@ function renderMealsData(meal) {
           </div>`;
 }
 
-function recipeModal(id) {
-  console.log("clicked",id);
-  
+function recipeModal(meal) {
+
+  const ingredients = [];
+
+  for(let i = 1; i <= 20; i++) {
+    const ingredient = meal[`strIngredient${i}`];
+    const measure = meal[`strMeasure${i}`];
+
+    if(ingredient) {
+      ingredients.push(`${measure ? measure : ""} ${ingredient}`.trim());
+    }
+  }
+
   renderModalData = `<!-- Modal container -->
-          <div class="bg-gray-800 rounded-lg shadow-lg w-full max-w-md relative">
+          <div class="bg-gray-800 rounded-lg shadow-lg w-full max-w-md relative h-3/4 overflow-y-scroll" style="
+              background: url('${meal?.strMealThumb}');
+              background-position: center center;
+              background-size: cover;
+            ">
             <!-- Close button -->
             <button
-              class="absolute cursor-pointer top-3 right-3 text-gray-500 hover:text-red-600 rounded-full p-2 transition"
+              class="absolute cursor-pointer top-3 right-3 text-gray-200 hover:text-red-600 rounded-full p-2 transition"
               onclick="document.getElementById('recipe-modal').classList.add('hidden')"
               aria-label="Close"
             >
@@ -106,28 +133,39 @@ function recipeModal(id) {
             </button>
 
             <!-- Modal content: Recipe details only -->
-            <div class="p-6 space-y-3">
-              <h2 class="text-2xl font-bold text-green-700">Classic Pasta</h2>
+            <div class="p-6 space-y-3 bg-gray-800/60" >
+              <h2 class="text-2xl font-bold text-white">${meal?.strMeal}</h2>
+              <div class="flex gap-3 items-center mb-4">
+                <h3 class="bg-gray-800/90 py-1 px-2 rounded-lg text-sm ${meal?.strCategory === "Vegetarian" ? "text-green-500" : meal?.strCategory === "Dessert" ? "text-green-500" :  "text-red-500"}">${meal?.strCategory}</h3>
+                ${meal?.strArea ? `&CenterDot; <h3 class=" bg-gray-800/90 py-1 px-2 rounded-lg text-sm">${meal.strArea}</h3>` : ""}
+                </div>
+                <div class="flex gap-2">${
+                  meal?.strTags
+                    ? meal.strTags
+                        ?.toString()
+                        .split(",")
+                        .map(
+                          (tag) =>
+                            `<span class="text-xs ring-2 ring-orange-600 hover:bg-orange-600 py-0.5 px-1 rounded-full">${tag}</span>`
+                        )
+                        .join(" ")
+                    : ""
+                }</div>
               <div>
                 <h3 class="font-semibold mb-1">Ingredients:</h3>
-                <ul class="list-disc list-inside text-gray-400 space-y-1">
-                  <li>200g spaghetti</li>
-                  <li>2 tbsp olive oil</li>
-                  <li>2 cloves garlic, minced</li>
-                  <li>Salt and pepper to taste</li>
+                <ul class="list-disc list-inside text-gray-200 space-y-1">
+                  ${ingredients.map(ingr => (
+                    `<li>${ingr}</li>`
+                  )).join(" ")}
                 </ul>
               </div>
               <div>
                 <h3 class="font-semibold mb-1">Instructions:</h3>
-                <ol class="list-decimal list-inside text-gray-400 space-y-1">
-                  <li>Boil spaghetti according to package instructions.</li>
-                  <li>Heat olive oil and sauté garlic until fragrant.</li>
-                  <li>
-                    Toss spaghetti in the pan, season with salt and pepper.
-                  </li>
-                  <li>Serve hot and enjoy!</li>
-                </ol>
+                <p>${meal?.strInstructions}</p>
               </div>
             </div>
           </div>`;
+
+    recipeModalContainer.classList.remove("hidden");
+    recipeModalContainer.innerHTML = renderModalData;
 }
